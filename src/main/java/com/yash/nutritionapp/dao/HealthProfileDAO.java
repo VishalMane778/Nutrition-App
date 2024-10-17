9a341dc6b80daf39a226bf9447792306e7a778ca
VishalMane778 <107835872+VishalMane778@users.noreply.github.com>
2024-10-17 15:19:06 +0530

    Delete src/main/java/com/yash/nutritionapp directory

--- src/main/java/com/yash/nutritionapp/dao/HealthProfileDAO.java
+++
@@ -1,9 +0,0 @@
-package com.yash.nutritionapp.dao;
-
-import com.yash.nutritionapp.domain.HealthProfile;
-
-import java.sql.SQLException;
-
-public interface HealthProfileDAO {
-    void saveHealthProfile(HealthProfile healthProfile) throws SQLException;
-}
